5249897ef01d7bfc1df3bcca90a5fed9ff310164
integerJI <jjs9536@gmail.com>
2021-01-10 20:38:32 +0900

    [Add] 회원가입 이동 추가

---
+++ project/src/main/java/com/project/controller/MemberController.java
@@ -0,0 +1,38 @@
+package com.project.controller;
+
+import javax.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.project.service.MemberService;
+
+
+@Controller
+public class MemberController {
+
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@Inject
+	MemberService service;
+	
+	// 회원가입 get
+	@RequestMapping(value = "/singup.do", method = RequestMethod.GET)
+	public String singup(Model model) {
+		logger.info(" ==============>>> SingUp Start !!!");
+		
+		return "member/singup";
+	}
+	
+//	// 회원가입 post
+//	@RequestMapping(value = "/register", method = RequestMethod.POST)
+//	public String postRegister(MemberVO vo) throws Exception {
+//		logger.info("post register");
+//		
+//		service.register(vo);
+//		
+//		return null;
+//	}
+}
--- project/src/main/webapp/WEB-INF/views/member/singup.jsp
+++ project/src/main/webapp/WEB-INF/views/member/singup.jsp
@@ -2,18 +2,9 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
+</html>
 <head>
-	<title>Home</title>
+	<h1>singup start</h1>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
-
-<a href="projectList.do">글 목록 이동</a>
-<a href="singup.do">글 목록 이동</a>
-
 </body>
-</html>
